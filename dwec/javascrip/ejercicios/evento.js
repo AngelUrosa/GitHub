9a80b41733757eaf4b4325fe4756aa8e8b4a12fc
AngelUrosa <dawaurosan@iescastelar.com>
2021-11-03 21:27:29 +0100

    aaa

---
+++ dwec/javascrip/ejercicios/evento.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+function inicializar(params) {
+    console.log('inicializar');
+}
+
+
+
+
+console.log("Inicio");
+
+window.addEventListener('load',inicializar);
+
+console.log("Fin");
